70c14a59053999ea49e6b4f600dc1a5f4ad06971
YdrMaster <ydrml@hotmail.com>
2023-08-31 15:38:16 +0800

    feat: 引入 onnx 的 protobuf 以解析 onnx
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ onnx/build.rs
@@ -0,0 +1,15 @@
+﻿use std::{env, io::Result, path::PathBuf};
+
+fn main() -> Result<()> {
+    let project = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let protoc = if cfg!(windows) {
+        "protoc/win64/protoc.exe"
+    } else if cfg!(linux) {
+        "protoc/linux-x86_64/protoc"
+    } else {
+        todo!("Os not supported")
+    };
+    std::env::set_var("PROTOC", project.join(protoc));
+    prost_build::compile_protos(&["onnx.proto3"], &["onnx-home/onnx/"])?;
+    Ok(())
+}
--- onnx/src/lib.rs
+++ onnx/src/lib.rs
@@ -29,3 +29,8 @@ pub struct Tensor {
 
 #[derive(Clone, Default, Debug)]
 pub struct Shape(smallvec::SmallVec<[i64; 4]>);
+
+#[allow(non_snake_case)]
+pub mod onnx {
+    include!(concat!(env!("OUT_DIR"), "/onnx.rs"));
+}
