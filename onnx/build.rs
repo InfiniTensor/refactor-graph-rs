9016c17dbcad4c956b66ce904c44009bb7291d33
YdrMaster <ydrml@hotmail.com>
2023-09-01 09:04:11 +0800

    fix: support linux
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- onnx/build.rs
+++ onnx/build.rs
@@ -2,12 +2,12 @@
 
 fn main() -> Result<()> {
     let project = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let protoc = if cfg!(windows) {
+    let protoc = if env::consts::OS == "windows" {
         "protoc/win64/protoc.exe"
-    } else if cfg!(linux) {
+    } else if env::consts::OS == "linux" {
         "protoc/linux-x86_64/protoc"
     } else {
-        todo!("Os not supported")
+        todo!("Os {} not supported", env::consts::OS);
     };
     std::env::set_var("PROTOC", project.join(protoc));
     prost_build::compile_protos(&["onnx.proto3"], &["onnx-home/onnx/"])?;
