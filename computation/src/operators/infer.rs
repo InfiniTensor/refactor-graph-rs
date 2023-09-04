c33b56f65176e4db01703ce12261a62b4031f5b8
YdrMaster <ydrml@hotmail.com>
2023-09-04 10:57:37 +0800

    refactor: 删除一些东西到能编译
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- computation/src/operators/infer.rs
+++ computation/src/operators/infer.rs
@@ -1,4 +1,4 @@
-use crate::edge::{Edge, Shape};
+use crate::Edge;
 
 pub type InferResult = Result<Vec<Edge>, InferError>;
 
@@ -7,47 +7,4 @@ pub enum InferError {
     SizeMismatch,
     DataTypeMismatch,
     BroadcastError,
-}
-
-/// Infer multidirectional broadcast.
-pub fn infer_multi_broadcast(shape_x: Shape, shape_y: Shape) -> Option<Shape> {
-    let mut broadcasted_shape = vec![];
-
-    let dims_x = shape_x.reverse_dims();
-    let dims_y = shape_y.reverse_dims();
-
-    for (dim_x, dim_y) in dims_x.iter().zip(dims_y.iter()) {
-        match (dim_x, dim_y) {
-            (Some(dim_x), Some(dim_y)) => {
-                if *dim_x == *dim_y {
-                    broadcasted_shape.push(Some(*dim_x))
-                } else if *dim_x == 1 {
-                    broadcasted_shape.push(Some(*dim_y))
-                } else if *dim_y == 1 {
-                    broadcasted_shape.push(Some(*dim_x))
-                } else {
-                    return None;
-                }
-            }
-            (Some(dim_x), None) => {
-                if *dim_x > 1 {
-                    broadcasted_shape.push(Some(*dim_x))
-                } else {
-                    broadcasted_shape.push(None)
-                }
-            }
-            (None, Some(dim_y)) => {
-                if *dim_y > 1 {
-                    broadcasted_shape.push(Some(*dim_y))
-                } else {
-                    broadcasted_shape.push(None)
-                }
-            }
-            (None, None) => broadcasted_shape.push(None),
-        }
-    }
-
-    broadcasted_shape.reverse();
-
-    Some(Shape::new(broadcasted_shape))
 }
--- computation/src/operators/mod.rs
+++ computation/src/operators/mod.rs
@@ -1,21 +1,13 @@
-﻿use crate::edge::Edge;
+﻿use crate::Edge;
 
-pub use self::element_wise::{ElementWise, ElementWiseType};
-pub use self::infer::{infer_multi_broadcast, InferError, InferResult};
+pub use self::infer::{InferError, InferResult};
 pub use self::op_type::OpType;
-pub use self::unary::{UnaryOperator, UnaryType};
 
-mod element_wise;
 mod infer;
-mod matmul;
 mod op_type;
-mod unary;
 
-pub trait OperatorInference {
-    fn infer(&self, inputs: &Vec<Edge>) -> InferResult;
-}
-
-pub struct Operator {
-    op_type: OpType,
-    info: Box<dyn OperatorInference>,
+/// 算子。
+pub trait Operator {
+    /// 根据算子输入推导输出。
+    fn infer(&self, inputs: &[Edge]) -> InferResult;
 }
